94484ac86ab7821a914c88127cd94f79ff79188e
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2021-02-01 17:56:08 +0100

    avances en app

---
+++ build/express.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+const config = require("./config.json");
+const express = require('express');
+const app = express();
+const port = 3000;
+const generador = new app_1.Generador();
+app.get('/', (req, res) => {
+    const algoritmo = config.algoritmo;
+    let maxNumber = '';
+    for (let i = 0; i < config.longitud; i++) {
+        maxNumber += '9';
+    }
+    if (config.cantidad > JSON.parse(maxNumber)) {
+        res.json({ error: 'error, debes indicar una cantidad menor respecto a la longitud de los cupones' });
+    }
+    else {
+        switch (algoritmo) {
+            case 'num secuencial':
+                res.json(generador.generateSecuencialNumber(config));
+                break;
+            case 'num random':
+                res.json(generador.generateRandomNumber(config));
+                break;
+            case 'alfa secuencial':
+                res.json(generador.generateAlfaNumber(generador.generateSecuencialNumber(config)));
+                break;
+            case 'alfa random':
+                res.json(generador.generateAlfaNumber(generador.generateRandomNumber(config)));
+                break;
+            default:
+                res.json({ error: 'ha habido un error con la cofiguracion del algoritmo' });
+        }
+    }
+});
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,47 @@
-function sayHello(name: string): string {
-  return 'Hi ' + name;
+const random = require('random')
+
+
+export class Generador {
+
+  generateSecuencialNumber(config): string[] {
+    const result: string[] = [];
+    for (let cupon = 1; cupon <= config.cantidad; cupon++) {
+      let cuponNumber: string = JSON.stringify(cupon);
+      const numberZeros: number = (config.longitud - cuponNumber.length)
+      for (let zero = 0; zero < numberZeros; zero++) {
+        cuponNumber = '0' + cuponNumber;
+      }
+      result.push(cuponNumber)
+    }
+    return result
+  }
+
+  generateAlfaNumber(generateNumber: string[]): string[] {
+    const numbers: string[] = generateNumber;
+    const alfaNumbers: string[] = []
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    numbers.forEach(number => {
+      const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+      number = `${randomCharacter}-${number}`;
+      alfaNumbers.push(number)
+    });
+    return alfaNumbers
+  }
+
+  generateRandomNumber(config): string[] {
+    const result: string[] = [];
+    for (let cupon = 1; cupon <= config.cantidad; cupon++) {
+      let lengthOfNumberString: string = '1';
+      for (let zero = 1; zero < config.longitud; zero++) {
+        lengthOfNumberString += '0'
+      }
+      let minLengthOfNumber: number = parseInt(lengthOfNumberString)
+      let maxLengthOfNumber: number = parseInt(lengthOfNumberString.replace('1', '9'))
+      let cuponNumber: number = random.int(minLengthOfNumber, maxLengthOfNumber);
+      result.push(JSON.stringify(cuponNumber))
+    }
+    return result
+  }
+
 }
 
-console.log(sayHello('Juan'))
